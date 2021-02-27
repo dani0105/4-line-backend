b2865ed91ed793a9368116c3b5a52599f7975d23
dani0105 <danielrojas0105@gmail.com>
2021-02-27 15:43:24 -0600

    Credenciales de la Base de datos

--- config/envconfig.js
+++ config/envconfig.js
@@ -1,9 +1,9 @@
 var config = {
     database: {
-        user: "",
-        host: "",
-        database: "",
-        password: "",
+        user: "oqvgxmjxrbiehz",
+        host: "ec2-54-162-119-125.compute-1.amazonaws.com",
+        database: "d1kjdjno27ao1m",
+        password: "6b26332e9fee7cbcbdd25e5c34b5937550a8fe536f04bded40cb832d3da9f9ac",
         port: 5432
     },
     server: {
