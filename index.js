5f5bae8a003ae67674427d011ab62f54483b58e7
dani0105 <danielrojas0105@gmail.com>
2021-04-01 20:05:06 -0600

    CORS error

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ app.use(express.json({ limit: '5mb' }));
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", req.headers.origin);
+    res.header("Access-Control-Allow-Origin", '*');
     res.header("Access-Control-Allow-Headers", "x-requested-with, content-type,Authorization");
     res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
     res.header("Access-Control-Allow-Credentials", "true");
