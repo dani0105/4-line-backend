2ccff6df56f39515b4a0d9b4071c0ba3bf99dafd
dani0105 <danielrojas0105@gmail.com>
2021-05-08 13:17:05 -0600

    Nuevos sockets

--- controllers/boardController.js
+++ controllers/boardController.js
@@ -12,7 +12,7 @@ exports.createGameRoom = (socket,client) => {
     client.on("createGameRoom", (data)=>{
         cont++;
         var code = Math.random().toString(36).substring(7).concat(cont);
-        var gameRoom = new NLineRoom(data.boardSize, 4, data.playerInfo, client, code, true, deleteGameRoom, data.bot, data.nivel);
+        var gameRoom = new NLineRoom(data.boardSize, 4, data.playerInfo, client, code, true, deleteGameRoom, data.bot, data.nivel,data.roomId);
         gameRooms.push(gameRoom);
         client.emit('createdGameRoom', code);
         if(data.bot){
@@ -59,9 +59,23 @@ exports.connectGameRoom = (socket,client) => {
  */
 exports.searchGame = (socket,client) => {
     client.on("searchGame", (data)=>{
+
         for( var i = 0; i < gameRooms.length; i++){
             var room = gameRooms[i];
-            if(!room.privateRoom && data.boardSize == room.board.length){
+            if(data.roomId){
+                if(room.roomId == data.roomId){
+                    room.player2 = {
+                        info:data.playerInfo,
+                        socket:client
+                    }
+                    client.emit('searchedGame',{searching:false,});
+                    room.start();
+                    return;
+                }
+                else
+                    continue;
+            }
+            if(!room.privateRoom && data.boardSize == room.board.length && room.roomId == null){
                 room.player2 = {
                     info:data.playerInfo,
                     socket:client
@@ -71,10 +85,80 @@ exports.searchGame = (socket,client) => {
                 return;
             }
         }
+
         cont++;
-        var gameRoom = new NLineRoom(data.boardSize,4, data.playerInfo, client,'',false,deleteGameRoom);
+        var gameRoom = new NLineRoom(data.boardSize,4, data.playerInfo, client,'',false,deleteGameRoom,false,0,data.roomId);
         gameRooms.push(gameRoom);
         client.emit('searchedGame', {searching:true});
     });
 }
+
+var rooms = [];
+
+exports.joinRoom = (socket,client) => {
+    client.on("joinRoom", (data)=>{
+
+        var room = null;
+        data.user.socket = client;
+        for(let i = 0; i < rooms.length; i++){
+            if(rooms[i].idRoom == data.idRoom){
+                var exist = false;
+                for(let i = 0; i < rooms[i].user.length; i++){
+                    if(rooms[i].user[i].id == data.user.id){
+                        exist = true;
+                        break
+                    }
+                }
+                
+                if(!exist)
+                    rooms[i].user.push(data.user);
+                room = rooms[i];
+                break
+            }
+        }
+        if(!room){
+            room = {idRoom:data.idRoom,user:[data.user]};
+            rooms.push(room);
+        }
+
+        client.join(data.idRoom);
+
+        //le envio al conectado los que estan en la sala conectados;
+        var users = [];
+        for(let i = 0; i < room.user.length; i ++){
+            const user = room.user[i]
+            users.push({
+                id:user.id,
+                username:user.username,
+                picture:user.picture
+            })
+        }
+        client.emit('roomInfo',users);
+
+        //enviar la informacion del conectado a todos
+        socket.in(data.idRoom).emit('userConnected',{
+            username: data.user.username,
+            id: data.user.id,
+            picture: data.user.picture,
+        });
+    });
+}
+
+
+exports.exitRoom = (socket,client) => {
+    client.on("exitRoom", (data)=>{
+        client.leave(data.idRoom);
+        socket.in(data.idRoom).emit('userDisconnected',data.user);
+    });
+}
+
+
+exports.exitRoom = (socket,client) => {
+    client.on(" ", (data)=>{
+        client.leave(data.idRoom);
+        socket.in(data.idRoom).emit('userDisconnected',data.user);
+    });
+}
+
+
 module.exports
--- index.js
+++ index.js
@@ -56,6 +56,8 @@ io./*use(/*Middlewares.verifyTokenSocket()).*/on("connection", (client) => {
     Controller.BoardController.createGameRoom(io, client);
     Controller.BoardController.connectGameRoom(io, client);
     Controller.BoardController.searchGame(io, client);
+    Controller.BoardController.joinRoom(io, client);
+    Controller.BoardController.exitRoom(io, client);
 });
 
 server.listen(process.env.PORT, function () {
